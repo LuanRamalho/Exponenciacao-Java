d21564ca2143801005042a786ecde74eba4ea119
Luan Ramalho <32373552+LuanRamalho@users.noreply.github.com>
2022-03-11 12:31:07 -0300

    Add files via upload

---
+++ Exponenciação.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+
+public class Exponenciação 
+{
+
+    
+    public static void main(String[] args) 
+    {
+        Scanner ler = new Scanner(System.in);
+        int num;
+        
+        System.out.println("Digite um número inteiro: ");
+        num = ler.nextInt();
+        
+        System.out.println("");
+        System.out.println("");
+        
+        System.out.println(""+num+ " elevado a"+ " 1 = "+ Math.pow(num,1));
+        System.out.println(""+num+ " elevado a"+ " 2 = "+ Math.pow(num,2));
+        System.out.println(""+num+ " elevado a"+ " 3 = "+ Math.pow(num,3));
+        System.out.println(""+num+ " elevado a"+ " 4 = "+ Math.pow(num,4));
+        System.out.println(""+num+ " elevado a"+ " 5 = "+ Math.pow(num,5));
+        System.out.println(""+num+ " elevado a"+ " 6 = "+ Math.pow(num,6));
+        System.out.println(""+num+ " elevado a"+ " 7 = "+ Math.pow(num,7));
+        System.out.println(""+num+ " elevado a"+ " 8 = "+ Math.pow(num,8));
+        System.out.println(""+num+ " elevado a"+ " 9 = "+ Math.pow(num,9));
+        System.out.println(""+num+ " elevado a"+ " 10 = "+ Math.pow(num,10));
+        
+        
+
+    }
+    
+}
